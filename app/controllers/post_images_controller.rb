64cb3d11199f36295b8daad2461130d562813cdc
EC2 Default User <yukkebbnba@gmail.com>
2021-06-03 07:35:45 +0000

    コメント機能の追加

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -16,9 +16,10 @@ class PostImagesController < ApplicationController
 
   def show
     @post_image = PostImage.find(params[:id])
+    @post_comment = PostComment.new
   end
 
- def destroy
+  def destroy
     @post_image = PostImage.find(params[:id])
     @post_image.destroy
     redirect_to post_images_path
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
  root to: 'homes#top'
  devise_for :users
- resources :post_images, only: [:new, :create, :index, :show, :destroy]
+ resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+  resources :post_comments, only: [:create, :destroy]
+ end
+ 
 end
