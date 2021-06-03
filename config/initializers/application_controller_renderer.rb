d1b03b9528a4aa7b4fc8eee8dd6bd82a82bccdb9
EC2 Default User <yukkebbnba@gmail.com>
2021-06-03 05:49:48 +0000

    3章おわり

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '0277b3e896e6d5c8d4e30852b7c6cab0444a9c324ea1ec79fd68ed9c088ee90ee42ce38ecbdb7275459461cb0d667ec0bd11c050e9b35b4a93c3c669e44ec807'
